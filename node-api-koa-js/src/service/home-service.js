7b46d887892c16afd95d61ab174b16ab20965962
ClearLove443 <1127280933@qq.com>
2023-05-06 14:53:30 +0800

    remove soe files

--- node-api-koa-js/src/service/home-service.js
+++
@@ -1,7 +0,0 @@
-class HomeService {
-  hello = () => {
-    return new Promise((resolve) => resolve("hello world"));
-  };
-}
-const homeService = new HomeService();
-export { homeService };
--- node-api-koa-ts/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import homeController from './controller/home-controller';
-
-export default [
-  {
-    path: '/test',
-    method: 'get',
-    action: homeController.hello,
-  },
-];
