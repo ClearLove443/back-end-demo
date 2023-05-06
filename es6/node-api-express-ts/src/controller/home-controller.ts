9d26fedf59ef0dd48c545b8bc8a2afdb2d5a4977
ClearLove443 <1127280933@qq.com>
2023-05-06 15:01:01 +0800

    add es6 node-api-express-ts

---
+++ es6/node-api-express-ts/src/controller/home-controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { homeService } from '../service/home-service.js';
+/**
+ * Adds two numbers together.
+ * @param {Request} req The first number.
+ * @param {Response} res The second number.
+ */
+class HomeController {
+  hello = (req: Request, res: Response) => {
+    homeService.hello(req, res);
+  };
+}
+
+const homeController = new HomeController();
+export { homeController };
+
---
+++ es6/node-api-express-ts/src/index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+import { PORT } from './config';
+import { AppRoutes } from './routes';
+
+const app = express();
+app.use('/', AppRoutes);
+app.listen(PORT);
+console.log(`应用启动成功 端口:${PORT}`);
